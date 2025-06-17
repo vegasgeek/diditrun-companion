99e97ac3551894b29bf8192907099bdc2131c620
John Hawkins <john@vegasgeek.com>
2025-06-16 19:12:44 -0700

    include github plugin header

--- diditrun-companion.php
+++ diditrun-companion.php
@@ -9,6 +9,7 @@
  * License: GPL v2 or later
  * License URI: https://www.gnu.org/licenses/gpl-2.0.html
  * Text Domain: diditrun-companion
+ * GitHub Plugin URI: https://github.com/vegasgeek/diditrun-companion
  *
  * @package diditrun-companion
  */
